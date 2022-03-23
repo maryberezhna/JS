a18f6e7d23def76dd8b49f71e5d43b8150ff9c12
Mariia Berezhna <mashaberezhna0209@gmail.com>
2022-03-23 17:59:43 +0200

    adding showMessage.js

---
+++ sum.js
@@ -0,0 +1,30 @@
+// function sum (a,b) {
+//   return a + b;   ///The directive return can be in any place of the function.
+// }
+// let  result = sum(5, 13);
+// alert(result);
+// ////
+
+function checkAge(age) {
+  if (age>=18) {
+    return true;
+  } else {
+    return confirm("Do you have permission from your parents?")
+  }
+}
+
+let age = prompt ("How old are you?", 18);  //Этот код отобразит модальное окно с текстом, полем для ввода текста и кнопками OK/Отмена.
+if (checkAge(age)){
+  alert("Access granted");
+} else{
+  alert("Access denied");
+}
+
+function showMovie(age) {
+  if ( !checkAge(age) ) {  //!=если не
+    return;   //It is possible to use return without a value. That causes the function to exit immediately.
+  }
+  alert( "Showing you the movie" );
+}
+/////
+//A function with an empty return or without it returns undefined//
