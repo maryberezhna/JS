6d4850a29b5f79472445584e334976488d290780
Mariia Berezhna <mashaberezhna0209@gmail.com>
2022-03-28 19:03:44 +0300

    adding the last version

---
+++ minJS.js
@@ -0,0 +1,12 @@
+function  min(a, b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+//or another variant//
+
+function min(a, b) {
+ return a < b ? a: b;
+}
+//read: compare and return a if true, b if false
