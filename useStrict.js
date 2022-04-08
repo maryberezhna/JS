7006484c387cb7c3cbcbf454dc6e3b37ca743d3f
Mariia Berezhna <mashaberezhna0209@gmail.com>
2022-04-08 18:15:04 +0300

    adding the last version

--- useStrict.js
+++
@@ -1,9 +0,0 @@
-alert("some code");
-'use strict';  //обычно люди используют его для всего файла.
-
-let admin;
-let name = "John";
-admin=name;
-alert(admin);
-
-//also you can do this  let admin, name;
